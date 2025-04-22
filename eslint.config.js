89305451b0b67674d6608b45a80e8bf2329e7cd5
Tramp <tarmpr77@icloud.com>
2025-04-23 01:29:09 +0300

    first commit

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,17 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+
+
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  eslintPluginUnicorn.configs.all,
+		{
+			rules: {
+				'unicorn/better-regex': 'warn',
+			},
+		},
 );
