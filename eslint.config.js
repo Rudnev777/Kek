2643ef679842f2d10d0711afdcbe7913d81c3ba4
sus-salmon <arsenii.kruglikov@brainsome.com>
2025-04-23 22:41:43 +0300

    Update ESLint config to include globals for node and es2022

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,19 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
-import eslintPluginUnicorn from 'eslint-plugin-unicorn';
-
-
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import globals from "globals";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   eslintPluginUnicorn.configs.all,
-		
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.es2022,
+      },
+    },
+  },
 );
