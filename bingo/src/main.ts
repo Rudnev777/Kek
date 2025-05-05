ef298cdcb6c8d4d63dc01d1118bba2f269bd2af7
Rudnev777 <tarmpr77@icloud.com>
2025-05-05 19:48:32 +0300

    bingo sketch

--- bingo/src/main.ts
+++ bingo/src/main.ts
@@ -1,3 +1,26 @@
 import './style.css';
 
-console.log('Hello, bingo!');
+const column = 5;
+const row = 9;
+
+const section = document.getElementById('grid');
+if (section) {
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < column; j++) {
+      const button = document.createElement('button');
+      button.className = 'button-playing-grid';
+      button.textContent = `${i} - ${j}`;
+      section.append(button);
+    }
+  }
+  section.style.gridTemplateColumns = `repeat(${column}, 1fr)`;
+  section.style.gridTemplateRows = `repeat(${row}, 1fr)`;
+}
+
+const arrayButton = document.querySelectorAll('.button-playing-grid');
+
+arrayButton.forEach(element => {
+  element.addEventListener('click', function (this: HTMLElement) {
+    this.style.backgroundColor = 'red';
+  });
+});
