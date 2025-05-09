17dbec07641a8c89a812e28d87b77ca8cdb9e37f
Rudnev777 <tarmpr77@icloud.com>
2025-05-09 10:08:00 +0300

    Bingo add field and logic and phrases (mark2)

--- bingo/src/main.ts
+++ bingo/src/main.ts
@@ -1,6 +1,6 @@
 import './style.css';
 
-const column = 2;
+const column = 4;
 const row = 3;
 const arrayPhrase = [
   'Понял понял',
@@ -17,26 +17,32 @@ const arrayPhrase = [
   'Не знаю',
 ];
 
-for (let index = 0; index < arrayPhrase.length; index++) {
-  const indexRandom = Math.floor(Math.random() * arrayPhrase.length);
+function shuffle(array: unknown[]): void {
+  for (let index = 0; index < array.length; index++) {
+    const indexRandom = Math.floor(Math.random() * array.length);
 
-  [arrayPhrase[index], arrayPhrase[indexRandom]] = [
-    arrayPhrase[indexRandom],
-    arrayPhrase[index],
-  ];
+    [array[index], array[indexRandom]] = [array[indexRandom], array[index]];
+  }
 }
+
+shuffle(arrayPhrase);
 
 let indexPhrase = 0;
 const section = document.querySelector<HTMLElement>('#grid');
+const matrix: HTMLElement[][] = [];
+
 if (section) {
   for (let indexRow = 0; indexRow < row; indexRow++) {
+    const arrayTemporary = new Array<HTMLElement>();
     for (let indexColumn = 0; indexColumn < column; indexColumn++) {
       const button = document.createElement('button');
       button.className = 'button-playing-grid';
       button.textContent = arrayPhrase[indexPhrase++];
       button.style.backgroundColor = 'white';
       section.append(button);
+      arrayTemporary.push(button);
     }
+    matrix.push(arrayTemporary);
   }
   section.style.gridTemplateRows = `repeat(${row}, 1fr)`;
   section.style.gridTemplateColumns = `repeat(${column}, 1fr)`;
@@ -45,63 +51,42 @@ if (section) {
 const arrayButton = document.querySelectorAll<HTMLElement>(
   '.button-playing-grid',
 );
-const matrix: HTMLElement[][] = [];
-
-for (let index = 0; index < row; index++) {
-  const arrayTemporary = new Array<HTMLElement>();
-  for (let indexTwo = 0; indexTwo < column; indexTwo++) {
-    arrayTemporary.push(arrayButton[index * column + indexTwo]);
-  }
-  matrix.push(arrayTemporary);
-}
+const arrayColor = ['white', 'green', 'yellow', 'orange', 'red'];
 let alertFinish = true;
+
 for (const element of arrayButton) {
-  element.addEventListener('click', () => {
-    const currentColor = element.style.backgroundColor;
-    if (currentColor === 'white') {
-      element.style.backgroundColor = 'green';
-    }
-    if (currentColor === 'green') {
-      element.style.backgroundColor = 'yellow';
-    }
-    if (currentColor === 'yellow') {
-      element.style.backgroundColor = 'orange';
-    }
-    if (currentColor === 'orange') {
-      element.style.backgroundColor = 'red';
-    }
-    if (finish(matrix) && alertFinish) {
+  const buttonObject = {
+    button: element,
+    buttonClick: 0,
+    buttonColor: function (color: string): void {
+      this.button.style.backgroundColor = color;
+    },
+  };
+  buttonObject.button.addEventListener('click', () => {
+    buttonObject.buttonColor(arrayColor[++buttonObject.buttonClick]);
+    if (hasCompleteRowOrColumn(matrix) && alertFinish) {
       alert('!!!BINGO!!!');
       alertFinish = false;
     }
   });
 }
 
-function finish(matrix: HTMLElement[][]): boolean {
-  let bool = true;
+function hasCompleteRowOrColumn(matrix: HTMLElement[][]): boolean {
+  let bool = false;
 
   for (const elementArray of matrix) {
-    bool = true;
-    for (const elementMatrix of elementArray) {
-      if (elementMatrix.style.backgroundColor === 'white') {
-        bool = false;
-        break;
-      }
-    }
+    bool = elementArray.every(
+      (element) => element.style.backgroundColor !== 'white',
+    );
     if (bool) {
       return bool;
     }
   }
 
   for (let index = 0; index < matrix[0].length; index++) {
-    bool = true;
-
-    for (const element of matrix) {
-      if (element[index].style.backgroundColor === 'white') {
-        bool = false;
-        break;
-      }
-    }
+    bool = matrix.every(
+      (element) => element[index].style.backgroundColor !== 'white',
+    );
     if (bool) {
       return bool;
     }
