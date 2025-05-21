1ac660c2b5a38f6aefb369601de1e65d075bc752
Rudnev777 <tarmpr77@icloud.com>
2025-05-22 01:39:42 +0300

    Bingo use in css-root in js-setProperty

--- bingo/src/main.ts
+++ bingo/src/main.ts
@@ -29,18 +29,38 @@ shuffle(arrayPhrase);
 
 let indexPhrase = 0;
 const section = document.querySelector<HTMLElement>('#grid');
-const matrix: HTMLElement[][] = [];
+
+type BingoCell = {
+  button: HTMLElement;
+  buttonClick: number;
+};
+
+const matrix: BingoCell[][] = [];
+const arrayColor = ['green', 'yellow', 'orange', 'red'];
+let alertFinish = true;
 
 if (section) {
   for (let indexRow = 0; indexRow < row; indexRow++) {
-    const arrayTemporary = new Array<HTMLElement>();
+    const arrayTemporary = new Array<BingoCell>();
     for (let indexColumn = 0; indexColumn < column; indexColumn++) {
-      const button = document.createElement('button');
-      button.className = 'button-playing-grid button-white';
-      button.textContent = arrayPhrase[indexPhrase++];
+      const buttonTemporary = document.createElement('button');
+      buttonTemporary.className = 'button-playing-grid button-play';
+      buttonTemporary.textContent = arrayPhrase[indexPhrase++];
 
-      section.append(button);
-      arrayTemporary.push(button);
+      section.append(buttonTemporary);
+      arrayTemporary.push({ button: buttonTemporary, buttonClick: 0 });
+      arrayTemporary[indexColumn].button.addEventListener('click', () => {
+        if (arrayTemporary[indexColumn].buttonClick < 4) {
+          arrayTemporary[indexColumn].button.style.setProperty(
+            '--button-color',
+            arrayColor[arrayTemporary[indexColumn].buttonClick++],
+          );
+        }
+        if (hasCompleteRowOrColumn(matrix) && alertFinish) {
+          alert('!!!BINGO!!!');
+          alertFinish = false;
+        }
+      });
     }
     matrix.push(arrayTemporary);
   }
@@ -48,49 +68,15 @@ if (section) {
   section.style.gridTemplateColumns = `repeat(${column}, 1fr)`;
 }
 
-const arrayColor = [
-  'button-white',
-  'button-green',
-  'button-yellow',
-  'button-orange',
-  'button-red',
-];
-let alertFinish = true;
-for (const elementArray of matrix) {
-  for (const element of elementArray) {
-    const buttonObject = {
-      buttonClick: 0,
-    };
-
-    element.addEventListener('click', () => {
-      if (buttonObject.buttonClick < 4) {
-        element.classList.toggle(arrayColor[buttonObject.buttonClick]);
-        element.classList.toggle(arrayColor[++buttonObject.buttonClick]);
-      }
-      if (hasCompleteRowOrColumn(matrix) && alertFinish) {
-        alert('!!!BINGO!!!');
-        alertFinish = false;
-      }
-    });
-  }
-}
-function hasCompleteRowOrColumn(matrix: HTMLElement[][]): boolean {
+function hasCompleteRowOrColumn(matrix: BingoCell[][]): boolean {
   for (const elementArray of matrix) {
-    if (
-      elementArray.every(
-        (element) => !element.classList.contains('button-white'),
-      )
-    ) {
+    if (elementArray.every((element) => element.buttonClick !== 0)) {
       return true;
     }
   }
 
   for (let index = 0; index < matrix[0].length; index++) {
-    if (
-      matrix.every(
-        (element) => !element[index].classList.contains('button-white'),
-      )
-    ) {
+    if (matrix.every((element) => element[index].buttonClick !== 0)) {
       return true;
     }
   }
