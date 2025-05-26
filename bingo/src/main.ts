c8b86bf7f5dcad6c5436b08ad502ee683052e712
Rudnev777 <tarmpr77@icloud.com>
2025-05-26 04:03:28 +0300

    Bingo add button copy and data

--- bingo/src/main.ts
+++ bingo/src/main.ts
@@ -1,5 +1,15 @@
 import './style.css';
 
+const clock = document.querySelector<HTMLElement>('#current-time');
+function currentTime() {
+  const data = new Date();
+  if (clock) {
+    clock.textContent = data.toDateString();
+  }
+}
+
+setInterval(currentTime, 1000);
+
 const column = 3;
 const row = 4;
 type phraseClue = {
@@ -29,87 +39,108 @@ function shuffle(array: unknown[]): void {
   }
 }
 
-shuffle(arrayPhrase);
+function bingoPlay() {
+  shuffle(arrayPhrase);
 
-let indexPhrase = 0;
-const section = document.querySelector<HTMLElement>('#grid');
+  let indexPhrase = 0;
+  const section = document.querySelector<HTMLElement>('#grid');
+  section?.replaceChildren();
+  type BingoCell = {
+    button: HTMLElement;
+    buttonClick: number;
+  };
 
-type BingoCell = {
-  button: HTMLElement;
-  buttonClick: number;
-};
+  const cells: BingoCell[][] = [];
+  const buttonPlayBingoColor = ['green', 'yellow', 'orange', 'red'];
+  const buttonPlayBingoChar = ['🟩', '🟨', '🟧', '🟥'];
+  const stringCell = Array.from({ length: row }, () =>
+    Array.from({ length: column }, () => '⬜️'),
+  );
 
-const cells: BingoCell[][] = [];
-const buttonPlayBingoColor = ['green', 'yellow', 'orange', 'red'];
-let alertWin = true;
-const stopColorChange = 4;
+  let alertWin = true;
+  const stopColorChange = 4;
 
-if (section) {
-  for (let indexRow = 0; indexRow < row; indexRow++) {
-    const arrayTemporary = new Array<BingoCell>();
-    for (let indexColumn = 0; indexColumn < column; indexColumn++) {
-      const buttonCell: BingoCell = {
-        button: document.createElement('button'),
-        buttonClick: 0,
-      };
+  if (section) {
+    for (let indexRow = 0; indexRow < row; indexRow++) {
+      const arrayTemporary = new Array<BingoCell>();
+      for (let indexColumn = 0; indexColumn < column; indexColumn++) {
+        const buttonCell: BingoCell = {
+          button: document.createElement('button'),
+          buttonClick: 0,
+        };
 
-      const originalText = arrayPhrase[indexPhrase].phrase;
-      const textClue = arrayPhrase[indexPhrase].clue;
-      buttonCell.button.textContent = arrayPhrase[indexPhrase++].phrase;
+        const originalText = arrayPhrase[indexPhrase].phrase;
+        const textClue = arrayPhrase[indexPhrase].clue;
+        buttonCell.button.textContent = arrayPhrase[indexPhrase++].phrase;
 
-      if (arrayPhrase[indexPhrase - 1].clue === '') {
-        buttonCell.button.className = 'button-cell';
-      } else {
-       
-        buttonCell.button.className = 'button-cell button-cell-clue';
-       buttonCell.button.addEventListener('mouseenter', () => {
-          buttonCell.button.textContent = textClue;
-        });
+        if (arrayPhrase[indexPhrase - 1].clue === '') {
+          buttonCell.button.className = 'button-cell';
+        } else {
+          buttonCell.button.className = 'button-cell button-cell-clue';
+          buttonCell.button.addEventListener('mouseenter', () => {
+            buttonCell.button.textContent = textClue;
+          });
 
-        buttonCell.button.addEventListener('mouseleave', () => {
-          buttonCell.button.textContent  = originalText;
+          buttonCell.button.addEventListener('mouseleave', () => {
+            buttonCell.button.textContent = originalText;
+          });
+        }
+
+        section.append(buttonCell.button);
+        arrayTemporary.push(buttonCell);
+
+        buttonCell.button.addEventListener('click', () => {
+          if (buttonCell.buttonClick < stopColorChange) {
+            stringCell[indexRow][indexColumn] =
+              buttonPlayBingoChar[buttonCell.buttonClick];
+
+            buttonCell.button.style.setProperty(
+              '--button-color',
+              buttonPlayBingoColor[buttonCell.buttonClick++],
+            );
+          }
+          if (alertWin && hasCompleteRowOrColumn(cells)) {
+            alert('!!!BINGO!!!');
+            alertWin = false;
+          }
         });
       }
-      
+      cells.push(arrayTemporary);
+    }
 
-      section.append(buttonCell.button);
-      arrayTemporary.push(buttonCell);
+    section.style.setProperty('--cells-bingo-play-rows', `repeat(${row}, 1fr)`);
+    section.style.setProperty(
+      '--cells-bingo-play-columns',
+      `repeat(${column}, 1fr)`,
+    );
 
-      buttonCell.button.addEventListener('click', () => {
-        if (buttonCell.buttonClick < stopColorChange) {
-          buttonCell.button.style.setProperty(
-            '--button-color',
-            buttonPlayBingoColor[buttonCell.buttonClick++],
-          );
+    function hasCompleteRowOrColumn(cells: BingoCell[][]): boolean {
+      for (const elementArray of cells) {
+        if (elementArray.every((element) => element.buttonClick !== 0)) {
+          return true;
         }
-        if (alertWin && hasCompleteRowOrColumn(cells)) {
-          alert('!!!BINGO!!!');
-          alertWin = false;
+      }
+
+      for (let index = 0; index < cells[0].length; index++) {
+        if (cells.every((element) => element[index].buttonClick !== 0)) {
+          return true;
         }
+      }
+
+      return false;
+    }
+
+    const buttonCopy = document.querySelector<HTMLElement>('#copy-button');
+    if (buttonCopy) {
+      buttonCopy.addEventListener('click', () => {
+        const finishString = stringCell.map((x) => x.join('')).join('\n');
+        void navigator.clipboard.writeText(finishString);
       });
     }
-    cells.push(arrayTemporary);
   }
-
-  section.style.setProperty('--cells-bingo-play-rows', `repeat(${row}, 1fr)`);
-  section.style.setProperty(
-    '--cells-bingo-play-columns',
-    `repeat(${column}, 1fr)`,
-  );
 }
 
-function hasCompleteRowOrColumn(cells: BingoCell[][]): boolean {
-  for (const elementArray of cells) {
-    if (elementArray.every((element) => element.buttonClick !== 0)) {
-      return true;
-    }
-  }
-
-  for (let index = 0; index < cells[0].length; index++) {
-    if (cells.every((element) => element[index].buttonClick !== 0)) {
-      return true;
-    }
-  }
-
-  return false;
-}
+bingoPlay();
+setInterval(() => {
+  bingoPlay();
+}, 86_400_000);
